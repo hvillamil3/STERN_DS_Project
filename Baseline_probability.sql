81fe0cc71ceb67845913e34e7bca01bfe5ba5fa1
Hans Villamil <hvillamil3@gmail.com>
2018-04-08 13:44:26 -0400

    split train and test from prior table

--- Baseline_probability.sql
+++ Baseline_probability.sql
@@ -1,12 +1,31 @@
+/***************
+Train order_id
+****************/
+
+select distinct a.order_id 
+--into #train_order_ids
+into [dbo].[order_products__prior_train]
+	from [dbo].[order_products__prior] a
+	where
+	not exists (select order_id from [dbo].[order_products__prior_test] g
+							where a.order_id = g.order_id )
+/*************/
+
 declare @user int
 set @user = 16000
 
---select  vv.[user_id] ,count(distinct product_id) as [number_of_products] 
---into #numberofproducts
---	from [dbo].[order_products__prior] h
---	left join dbo.orders vv on h.[order_id]=vv.[order_id]
---	where  vv.[user_id] = @user
---	group by  vv.[user_id] 
+select  vv.[user_id] ,count(distinct product_id) as [number_of_products]
+into #numberofproducts
+	from (select *
+	from [dbo].[order_products__prior] j
+	where EXISTS (select 1
+				from #train_order_ids m
+				where j.order_id=m.order_id)
+
+	) as h
+	left join dbo.orders vv on h.[order_id]=vv.[order_id]
+	--where  vv.[user_id] = @user
+	group by  vv.[user_id] 
 
 --select top 100* from #numberofproducts
 
@@ -27,12 +46,15 @@ select b.[user_id]
 --, count(*) OVER ()  as Total_Number_of_products_purchased1
 --, count(*) OVER (PARTITION BY b.[user_id])==  max(gg.[number_of_products]) as Total_Number_of_products_purchased
 , max(gg.[number_of_products])  as Total_Number_of_products_purchased
---into frequency_probability 
+into frequency_probability 
 from [dbo].[order_products__prior] a
 left join dbo.orders b on a.[order_id]=b.[order_id]
 left join #numberofproducts as gg
 on b.[user_id]=gg.[user_id]
 --where b.[user_id] in (2,193700) --= @user --193700
+WHERE EXISTS (select 1
+				from #train_order_ids m
+				where a.order_id=m.order_id)
 group by b.[user_id],product_id
 --order by count(*) desc
 
